6973e3c662c5f33b4110fabf80b34d5cdac3ee05
joshvajaspher6-dotcom <joshvajaspher6@gmail.com>
2026-08-19 22:38:27 +0530

    assertion added

--- pages/cart.py
+++ pages/cart.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page,expect
 
 class Cart:
     def __init__(self,page: Page):
@@ -6,6 +6,8 @@ class Cart:
         self.check_out_button =             self.page.locator('a:has-text("Proceed To Checkout")')
         self.cart_products    =             self.page.locator('tbody tr')
         self.remove_product_cart =          self.page.locator('[class="cart_quantity_delete"]')
+        self.checkout =                     self.page.get_by_text("Proceed To Checkout")
+        self.checkout_price =               self.page.locator('[class="cart_total_price"]')
 
     def click_check_out_button(self):
         self.check_out_button.click()
@@ -18,3 +20,32 @@ class Cart:
 
     def get_remove_product_locator(self):
         return self.remove_product_cart
+
+    def get_product_price(self):
+        return self.product_price
+
+    def get_product_quantity(self):
+        return self.product_quantity
+
+    def get_total_product(self):
+        return self.product_total_price
+
+    def calculate_total_price(self,product):
+        product_price = product.locator('[class="cart_price"]').inner_text()
+        product_quantity = product.locator('[class="cart_quantity"]').inner_text()
+        product_total_price = product.locator('[class="cart_total"]').inner_text()
+
+        price = int(product_price.replace("Rs.","").strip())
+        quantity = int(product_quantity.strip())
+        total_price = int(product_total_price.replace("Rs."," ").strip())
+        assert price*quantity == total_price
+
+        return total_price
+
+    def get_checkout(self):
+        self.checkout.click()
+
+    def get_checkout_price(self):
+        checkout_price = self.checkout_price.last.inner_text()
+        return int(checkout_price.replace("Rs."," ").strip())    
+
--- tests/test_cart.py
+++ tests/test_cart.py
@@ -11,11 +11,6 @@ def test_product_cart_with_one_product(page):
     cart_page = Cart(page)
     base_page = BasePage(page)
     product_page = ProductsPage(page)
-    # base_page.click_signup_login()
-    # login_page = LoginSignupPage(page)
-    # base_page.click_signup_login()
-    # login_page.login("jane.doe.qa01@xample.com", "SecurePass@123")
-
     base_page.click_product()
     product_page.product_search_fill("top")
     product = product_page.add_to_cart_button()
@@ -55,11 +50,21 @@ def test_product_cart_with_multiple_products(page):
         product_page.click_continue_shopping()
     base_page.click_cart()
     expect(cart_page.cart_products).to_have_count(len(product_list))
+
+    total_price = 0
+    for el in cart_page.get_products_cart():
+        price = cart_page.calculate_total_price(el)
+        total_price += price
+   
+    cart_page.get_checkout()
+    checkout_price = cart_page.get_checkout_price()
+    assert total_price == checkout_price 
+
+    base_page.click_cart()
     remove_product = cart_page.get_remove_product()
     for el in remove_product:
         el.click()
-    page.wait_for_timeout(10000)    
-    
+   
 
 
         
